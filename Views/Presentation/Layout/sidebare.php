b8896a6875c4cfc51fced867ef139f974a7950f1
Amina Assaid <aminaassaid123@gmail.com>
2023-10-13 13:06:22 +0100

    add

---
+++ Views/Presentation/Layout/sidebare.php
@@ -0,0 +1,49 @@
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+<a href="#" class="brand-link">
+<span class="brand-text font-weight-light">Arbre des Compétences</span>
+</a>
+
+<div class="sidebar">
+
+<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+
+<div class="info">
+<a href="#" class="d-block">Admin</a>
+</div>
+</div>
+
+
+
+<nav class="mt-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-tachometer-alt"></i>
+<p>
+Gestion Compétences 
+</p>
+</a>
+
+<li class="nav-item">
+<a href="../widgets.html" class="nav-link">
+<i class="nav-icon fas fa-th"></i>
+<p>
+Gestions Des Niveaux</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-copy"></i>
+<p>
+Gestion Formateurs
+
+<span class="badge badge-info right">5</span>
+</p>
+</a>
+
+
+
+</aside>
